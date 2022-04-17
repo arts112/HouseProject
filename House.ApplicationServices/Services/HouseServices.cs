eb733dd545b6e6555ad09be1d2a4b375ae865f04
ArthurThomson <artsthomson@gmail.com>
2022-04-17 13:25:22 +0300

    creating service core and data projects

---
+++ House.ApplicationServices/Services/HouseServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House.ApplicationServices.Services
+{
+    class HouseServices
+    {
+    }
+}
