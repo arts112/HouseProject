13eec9c04af231695198b878f6d1b307e71200c6
ArthurThomson <artsthomson@gmail.com>
2022-04-17 13:08:57 +0300

    Add project files.

---
+++ HouseProject/Models/House/HouseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseProject.Models.House
+{
+    public class HouseViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Aadress { get; set; }
+        public string Size { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
