89769f337988dfa62608e4defa81ff93e75d5e91
ArthurThomson <artsthomson@gmail.com>
2022-05-17 00:02:31 +0300

    Migration added and database creation, view configuration.

--- House.Core/Domain/Houses.cs
+++ House.Core/Domain/Houses.cs
@@ -5,7 +5,7 @@ namespace House.Core.Domain
 {
     public class Houses
     {
-        [Key]
+        
         public Guid? Id { get; set; }
         public string Address { get; set; }
         public double Size { get; set; }
